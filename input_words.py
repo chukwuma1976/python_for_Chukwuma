59773d2ba4a3ee577e0680f75ae6870ca0ac086a
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2023-09-20 16:16:59 -0400

    added print on one line function

--- input_words.py
+++ input_words.py
@@ -13,14 +13,20 @@ def print_all_reversed(array):
     for word in array:
         print(word[::-1])
 
+def print_on_one_line(array):
+    line = ""
+    for word in array:
+        line = line + word + " "
+    print(line)
+
 while word.lower() != "exit":
     word = input("Please enter a word to add to the array or enter \"exit\": ")
     if word.lower() != "exit": wordsarray.append(word)
 
 print("======================================================================================================")
 print("\t Words array")
-for word in wordsarray:
-    print(word)
+
+print(wordsarray)
 
 print("======================================================================================================")
 
@@ -32,7 +38,8 @@ while choice != '0' :
 Enter: 
     1 for ALL CAPS, 
     2 for ALL LOWERCASE, 
-    3 for ALL REVERSED
+    3 for ALL REVERSED,
+    4 to PRINT ON ONE LINE
     OR 0 to exit: """)
     if choice == '1':
         print_all_caps(wordsarray)  
@@ -40,6 +47,8 @@ Enter:
         print_all_lowercase(wordsarray)
     elif choice == '3':
         print_all_reversed(wordsarray)
+    elif choice == '4':
+        print_on_one_line(wordsarray)
     print("======================================================================================================")
 
 
